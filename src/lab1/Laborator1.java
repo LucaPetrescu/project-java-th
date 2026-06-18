c07e80213a2f381f99e2c327b902b742af0d494a
Luca Petrescu <luca.petrescu@inthepocket.com>
2026-06-18 18:46:49 +0300

    feat: add first lab

---
+++ src/lab1/Laborator1.java
@@ -0,0 +1,10 @@
+package lab1;
+
+public class Laborator1 {
+
+    public static void predictOutput() {
+        System.out.println(0.1 + 0.2);
+        System.out.println(Integer.MAX_VALUE + 1);
+    }
+
+}
