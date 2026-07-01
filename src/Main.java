8542a458b4398324ec251bfae45fc5253993c5b6
LucaPetrescu <luca6011@gmail.com>
2026-07-02 01:25:47 +0300

    feat: lab 1 ex 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
+import lab1.Laborator1;
+
 public class Main {
     public static void main(String[] args) {
        
+        // --------------------------------------------Laborator 1--------------------------------------------------------
+
+        // Ex1
+        System.out.println(Laborator1.reverse(123));
+
     }
 }
--- src/lab1/Laborator1.java
+++ src/lab1/Laborator1.java
@@ -2,9 +2,24 @@ package lab1;
 
 public class Laborator1 {
 
-    public static void predictOutput() {
-        System.out.println(0.1 + 0.2);
-        System.out.println(Integer.MAX_VALUE + 1);
+    // Exercise 1
+    public static int reverse(int x) {
+
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+    
+        long reverseNumber = 0;
+
+        while(x != 0) {
+            reverseNumber = reverseNumber * 10 + (x % 10); 
+            x = x / 10;
+        }
+
+        if(reverseNumber > max || reverseNumber < min) {
+            return 0;
+        }
+
+        return (int) reverseNumber;
     }
 
 }
